0714cf934137b687895dcb365cc2ea9792814e99
vladivhub <sumet@abv.bg>
2021-01-26 23:48:15 +0200

    Create Bus.py

---
+++ Bus.py
@@ -0,0 +1,12 @@
+passengers = int(input())
+stops = int(input())
+
+for i in range(1, stops + 1):
+    passengers_off = int(input())
+    passengers_on = int(input())
+    if i % 2 != 0:
+        passengers = passengers - passengers_off + passengers_on + 2
+    elif i % 2 == 0:
+        passengers = passengers - passengers_off + passengers_on - 2
+
+print(f"The final number of passengers is : {passengers}")
